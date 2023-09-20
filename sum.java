99fd4235997d632821c004d3dda40a8307faf077
10kevin77 <145546452+10kevin77@users.noreply.github.com>
2023-09-20 15:05:27 +0530

    Sum

---
+++ sum.java
@@ -0,0 +1,13 @@
+package javaprojects;
+
+public class sum {
+	public static void main(String [] args)
+    {
+		int Number1=20,Number2=10;
+		int sum=Number1+Number2;
+		System.out.println ("The sum is:"+sum);
+		
+		
+		
+    }
+}
