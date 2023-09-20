9530465e18a4f0ec3b085de192f3800182dddaac
10kevin77 <145546452+10kevin77@users.noreply.github.com>
2023-09-20 15:05:51 +0530

    product

---
+++ product.java
@@ -0,0 +1,11 @@
+package javaprojects;
+
+public class product {
+	public static void main(String [] args)
+	{
+		int Number1=20,Number2=20;
+		int product=Number1*Number2;
+		System.out.println ("The product is:"+product);
+	}
+
+}
