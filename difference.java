e385d3d83b6159f2f37e10fa15592865dbfa7157
10kevin77 <145546452+10kevin77@users.noreply.github.com>
2023-09-20 15:03:57 +0530

    Difference

---
+++ difference.java
@@ -0,0 +1,10 @@
+package javaprojects;
+
+public class difference {
+	public static void main(String [] args)
+	{
+	int Number1=20,Number2=20;
+	int diff=Number1-Number2;
+	System.out.println ("The differense is:"+diff);
+	}
+}
