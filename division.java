18a56ac1a5e3f092a7397cf3f1df32ab34772a08
10kevin77 <145546452+10kevin77@users.noreply.github.com>
2023-09-20 15:11:03 +0530

    division

---
+++ division.java
@@ -0,0 +1,11 @@
+package javaprojects;
+
+public class division {
+	public static void main(String [] args)
+	{
+
+		int Number1=10,Number2=20;
+		float result=Number1/Number2;
+		System.out.println ("The result is:"+result);
+	}
+}
